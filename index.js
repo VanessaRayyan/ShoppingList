8d069c92049e53e44efc84104b287ad003aa04a2
Vanessa Rayyan <vanessarayyan@outlook.com>
2020-05-06 20:23:05 -0400

    Changed to this keyword

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ $('#js-shopping-list-form').submit( event => {
 });
 
 $(".shopping-list").on ('click', '.shopping-item-toggle', function(e) {
-    let item = $(e.currentTarget).closest("li").find(".shopping-item");
+    let item = $(this).closest("li").find(".shopping-item");
     item.toggleClass('shopping-item__checked');
 });
 
-$(".shopping-list").on ('click','.shopping-item-delete', event => {
-  $(event.currentTarget).closest("li").remove();
+$(".shopping-list").on ('click','.shopping-item-delete', function(e) {
+  $(this).closest("li").remove();
 }); 
